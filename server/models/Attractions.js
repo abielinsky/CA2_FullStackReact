c51bba36358ebe78fc26a9d4e0433912d040df89
Lee Xuan Ong <d00253282@student.dkit.ie>
2022-12-01 20:33:33 +0800

    update models format

--- server/models/Attractions.js
+++ server/models/Attractions.js
@@ -3,10 +3,12 @@ const mongoose = require(`mongoose`)
 let AttractionsSchema = new mongoose.Schema(
     {
         name: {type: String},
-        addressLocality: {type: String},
-        addressRegion: {type: String},
+        address: {
+            addressLocality: {type: String},
+            addressRegion: {type: String}
+        },
         url: {type: String},
-        tags: {type: String},
+        tags: {type: Array},
         telephone: {type: Number},
 
     },
